6aa911e182b8d336c38c4659b14c0a4b634baaa0
Kailash Choudhary <48403668+kailash8@users.noreply.github.com>
2024-07-25 16:47:40 +0000

    ok

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print('This is my first file in replit.')
-print(5)
+# print("Hello World. \nhello")
+# print(9)
+# 
+# a = 'string'
+# b = "string"
+# c = '''string'''
+# d = True
+# e = False
+# 
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# 
+# print(type(int("2") + float("12.0")))
+
+user_input = input("Enter anything : ")
+print("User Input data is :" , user_input)
