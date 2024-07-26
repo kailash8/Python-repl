ef60fb43f36650c5d0cff9aa1ee3b8a7856cf94a
Kailash Choudhary <48403668+kailash8@users.noreply.github.com>
2024-07-26 17:57:57 +0000

    ok

--- main.py
+++ main.py
@@ -15,17 +15,31 @@
 #
 # print(type(int("2") + float("12.0")))
 
-user_input = input("Enter anything : ")
-print("User Input data is :", user_input)
+# user_input = input("Enter anything : ")
+# print("User Input data is :", user_input)
+#
+# p = """
+# Hi
+# Me
+# Here
+# Hello
+# I
+# Me
+# am
+# """
+#
+# print(p)
 
-p = """
-Hi
-Me
-Here
-Hello
-I
-Me
-am
-"""
 
-print(p)
+def rotate_array(arr, k):
+  n = len(arr)
+  k = k % n
+  arr = arr[n - k:] + arr[:n - k]
+  return arr
+
+
+arr = [1, 2, 3, 4, 5]
+k = 2
+rotated_arr = rotate_array(arr, k)
+print(f"Input array: {arr}")
+print(f"Rotated array: {rotated_arr}")
