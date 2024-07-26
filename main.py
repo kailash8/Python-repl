0d5c02a3008d5af554bf44b77cbdfb2233a47a22
Kailash Choudhary <48403668+kailash8@users.noreply.github.com>
2024-07-26 17:53:20 +0000

    ok

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 # print("Hello World. \nhello")
 # print(9)
-# 
+#
 # a = 'string'
 # b = "string"
 # c = '''string'''
 # d = True
 # e = False
-# 
+#
 # print(type(a))
 # print(type(b))
 # print(type(c))
 # print(type(d))
 # print(type(e))
-# 
+#
 # print(type(int("2") + float("12.0")))
 
 user_input = input("Enter anything : ")
-print("User Input data is :" , user_input)
+print("User Input data is :", user_input)
+
+p = """
+Hi
+Me
+Here
+Hello
+I
+Me
+am
+"""
+
+print(p)
