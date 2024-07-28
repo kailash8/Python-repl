e87f53b22ee5b664db020d92752b4b6abb4a67c1
Kailash Choudhary <48403668+kailash8@users.noreply.github.com>
2024-07-28 06:34:22 +0000

    string methods

--- main.py
+++ main.py
@@ -37,9 +37,43 @@ def rotate_array(arr, k):
   arr = arr[n - k:] + arr[:n - k]
   return arr
 
-
+'''
 arr = [1, 2, 3, 4, 5]
 k = 2
 rotated_arr = rotate_array(arr, k)
 print(f"Input array: {arr}")
 print(f"Rotated array: {rotated_arr}")
+'''
+
+# p = """
+# Hi
+# Me
+# Here
+# Hello
+# I
+# Me
+# am
+# """
+#
+# for x in p :
+#   print(x)
+
+
+# Python String Methonds
+
+str = 'all string Methods returns new values. They do NOT change the original string.'
+
+print("\nInput String :\n" ,str)
+print('###############')
+#print(str.capitalize())
+#print(str.casefold())
+#p = repr(str.center(20))
+#print(str.count('o'))
+#print(str.encode())
+#print(str.endswith('.'))
+#print(str.expandtabs(2
+#print(str.find("NOT"))
+#print("I {} here.".format('was'))
+data = {"name" : "kailash", "age" : 90}
+#print("My name is {} and I am {} years old.".format(data["name"], data["age"]))
+print(str.index("NOT"))
